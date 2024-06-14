da7d999c577a00efeb4920511d665e0c15c46fc6
Pieter Hoppenbrouwers <pieterhoppenbrouwers@hotmail.com>
2024-06-14 17:54:53 +0200

    listing object

---
+++ listing.py
@@ -0,0 +1,12 @@
+class Listing:
+    def __init__(self, address, price, location, start_date, end_date=None, platform=None, square_meters=None):
+        self.address = address
+        self.price = price
+        self.location = location
+        self.start_date = start_date
+        self.end_date = end_date
+        self.platform = platform
+        self.square_meters = square_meters
+
+    def __str__(self):
+        return f"{self.title} - {self.price} - {self.location}"
